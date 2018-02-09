4ebdc34ffc9ca711689f8ad58bfffd892f5627ec
Ilya Kremniou <Lolik21@users.noreply.github.com>
2018-02-09 13:16:53 +0300

    Revert "Revert "Commit""
    This reverts commit 3a253f23612eabc357d43002c172278835730c5f, reversing
    changes made to f706a7f2d12249c6b8f1e6e5429f76258efd9293.

--- K-Means/K-Means/KClass.cs
+++ K-Means/K-Means/KClass.cs
@@ -1,12 +1,35 @@
-﻿using System.Collections.Generic;
-using System.Windows;
-using System.Windows.Documents;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="KClass.cs" company="no">
+// no  
+// </copyright>
+// <summary>
+//   Defines the KClass type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace K_Means
 {
+    using System.Collections.Generic;
+    using System.Windows;
+
+    /// <summary>
+    /// The k class.
+    /// </summary>
     public class KClass
     {
+        /// <summary>
+        /// Gets or sets the points.
+        /// </summary>
         public List<Point> Points { get; set; }
+
+        /// <summary>
+        /// Gets or sets the center.
+        /// </summary>
         public Point Center { get; set; }
+
+        /// <summary>
+        /// Gets or sets the far.
+        /// </summary>
+        public Point Far { get; set; }
     }
 }
--- K-Means/K-Means/PointHelper.cs
+++ K-Means/K-Means/PointHelper.cs
@@ -1,24 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Windows;
-using System.Windows.Documents;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PointHelper.cs" company="no">
+//   no
+// </copyright>
+// <summary>
+//   The point helper.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace K_Means
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Windows;
+
+    /// <summary>
+    /// The point helper.
+    /// </summary>
     public static class PointHelper
     {
+        /// <summary>
+        /// The get random points.
+        /// </summary>
+        /// <param name="count">
+        /// The count.
+        /// </param>
+        /// <param name="canvasHeight">
+        /// The canvas height.
+        /// </param>
+        /// <param name="canvasWidth">
+        /// The canvas width.
+        /// </param>
+        /// <returns>
+        /// The <see>
+        ///         <cref>IEnumerable</cref>
+        ///     </see>
+        ///     .
+        /// </returns>
         public static IEnumerable<Point> GetRandomPoints(int count, int canvasHeight, int canvasWidth)
         {
             var random = new Random();
             List<Point> pointList = new List<Point>(count);
             for (int i = 0; i < count; i++)
             {
-                pointList.Add(new Point(random.Next(canvasWidth),random.Next(canvasHeight)));
+                pointList.Add(new Point(random.Next(canvasWidth), random.Next(canvasHeight)));
             }
+
             return pointList;
         }
 
+        /// <summary>
+        /// The get points distance.
+        /// </summary>
+        /// <param name="first">
+        /// The first.
+        /// </param>
+        /// <param name="second">
+        /// The second.
+        /// </param>
+        /// <returns>
+        /// The <see cref="double"/>.
+        /// </returns>
         public static double GetPointsDistance(Point first, Point second)
         {
             var dx = first.X - second.X;
@@ -26,9 +67,21 @@ namespace K_Means
             return Math.Sqrt(dx * dx + dy * dy);
         }
 
+        /// <summary>
+        /// The get nearest point.
+        /// </summary>
+        /// <param name="point">
+        /// The point.
+        /// </param>
+        /// <param name="points">
+        /// The points.
+        /// </param>
+        /// <returns>
+        /// The <see cref="Point"/>.
+        /// </returns>
         public static Point GetNearestPoint(Point point, List<Point> points)
         {
-            var min = Double.MaxValue;
+            var min = double.MaxValue;
             var nearestPoint = new Point();
             foreach (var p in points)
             {
@@ -42,6 +95,5 @@ namespace K_Means
 
             return nearestPoint;
         }
-
     }
 }
